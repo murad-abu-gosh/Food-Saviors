e8de9339c2f8894115159080b86dde5d375d5eb6
Murad <muradabgosh@gmail.com>
2022-03-27 04:59:51 +0300

    Added Login Page

---
+++ src/core/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper'
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#000000',
+    primary: '#5AF142',
+    secondary: '#f14757',
+    error: '#f13a59',
+  },
+}
