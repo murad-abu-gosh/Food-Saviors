9d54116b971ecb31bfa332c6e24d4d7a3b4f9eac
Murad <muradabgosh@gmail.com>
2022-05-25 23:33:35 +0300

    filename from 15 to 20

--- src/config/database_interface.js
+++ src/config/database_interface.js
@@ -396,7 +396,7 @@ function generateName() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 15; i++)
+  for (var i = 0; i < 20; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
