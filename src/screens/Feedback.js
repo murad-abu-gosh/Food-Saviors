f94de15310e8498d3f6756b598295288a96d2168
Muath Abu Jamal <muathsoftware@gmail.com>
2022-06-10 09:33:44 +0300

    feed back and icons commit

--- src/screens/Feedback.js
+++ src/screens/Feedback.js
@@ -47,7 +47,7 @@ export default function Feedback({ navigation, route }) {
 
           newFeedbacksList.push(currFeedbackInfoObj);
           isSuitable = true;
-          break;
+          break; 
         }
       }
 
